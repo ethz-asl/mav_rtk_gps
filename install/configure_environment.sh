e706a2d50ad6557db69f0bc3bc8dfa61fcef8a04
marco-tranzatto <marco.tranzatto@gmail.com>
2016-12-19 14:45:46 +0100

    Fixed install script

--- install/configure_environment.sh
+++ install/configure_environment.sh
@@ -10,10 +10,17 @@ cd ./libsbp
 git checkout $REPO_TAG
 
 # Install requirements.
+cd ./python
 sudo apt-get install pandoc
+sudo pip install tox
 sudo pip install -r requirements.txt
+sudo pip install markupsafe
+sudo python setup.py install
 # Build package.
-make python
+cd ..
+sudo make python
+
+echo "Build completed"
 
 # Export PYTHONPATH and make sure it points to the python subdirectory of the repository
 sh -c 'echo "export PYTHONPATH=\${PYTHONPATH}:~/piksi_rtk_lib/libsbp/python #add libsbp for RTK GPS Piksi devices" >> ~/.bashrc'
